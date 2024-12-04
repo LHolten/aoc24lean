32326450c1114c0e850966de3f4e98d8ffdd00d7
Lean 4 VS Code Extension <>
2024-12-04 10:19:59 +0100

    Initial commit

---
+++ Aoc24lean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Aoc24lean` library.
+-- Import modules here that should be built as part of the library.
+import Aoc24lean.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Aoc24lean
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
